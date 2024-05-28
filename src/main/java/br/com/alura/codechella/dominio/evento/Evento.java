a9116456f077032f1dc7005841db62838cd7981f
fabiosimplis <fabio.simplis@gmail.com>
2024-05-28 17:49:41 -0300

    Implementando ao Domínio.

---
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -0,0 +1,43 @@
+package br.com.alura.codechella.dominio.evento;
+
+import br.com.alura.codechella.Categoria;
+import br.com.alura.codechella.DadosEndereco;
+import br.com.alura.codechella.dominio.ingresso.TipoIngresso;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Evento {
+
+    private Categoria categoria;
+    private String descricao;
+    private DadosEndereco endereco;
+    private LocalDateTime data;
+    private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public DadosEndereco getEndereco() {
+        return endereco;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public List<TipoIngresso> getTipoIngressos() {
+        return tipoIngressos;
+    }
+
+    public void incluiNovoTipoDeIngresso(TipoIngresso tipoIngresso){
+        this.tipoIngressos.add(tipoIngresso);
+    }
+
+}
